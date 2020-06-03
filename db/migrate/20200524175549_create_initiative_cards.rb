62610738024dae1ad91c46d5f4790700b56bb10f
Terence Lee <hone02@gmail.com>
2020-06-03 02:39:09 -0500

    attribute is reserved by ActiveRecord

--- db/migrate/20200524175549_create_initiative_cards.rb
+++ db/migrate/20200524175549_create_initiative_cards.rb
@@ -2,7 +2,7 @@ class CreateInitiativeCards < ActiveRecord::Migration[6.0]
   def change
     create_table :initiative_cards do |t|
       t.string :name
-      t.string :attribute
+      t.string :subtitle
 
       t.timestamps
     end
